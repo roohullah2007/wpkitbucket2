a530082135a147dd9b112edea106333f5a434461
macbook air <macbookair@macbooks-MacBook-Air.local>
2026-08-25 03:12:24 +0500

    Security: strip injected build-config payload

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 
+
+
 export default {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
     },
-};                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+};
